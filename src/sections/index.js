172b596da79a994c88f0669adaeda596dd4222ca
Subashkumar-S <subashs2232@gmail.com>
2023-08-21 13:00:28 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Footer from "./Footer";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  Footer,
+  SpecialOffer,
+  Subscribe,
+  SuperQuality,
+};
